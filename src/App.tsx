4d55635b9129ccc2e2e0eacd7bb27dd5f08e8b5f
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-09-12 05:02:57 +0700

    14- Setup AdminLayout using CSS grid & Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import cityApi from 'api/cityApi';
-import { useAppDispatch } from 'app/hooks';
 import { NotFound, PrivateRoute } from 'components/Common';
 import { AdminLayout } from 'components/Layout';
-import { authActions } from 'features/auth/authSlice';
 import LoginPage from 'features/auth/pages/LoginPage';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Button } from '@material-ui/core';
 
 function App() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    cityApi.getAll().then((response) => console.log(response));
-  });
-
   return (
     <>
-      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>
-        Logout
-      </Button>
       <Switch>
         <Route path="/login">
           <LoginPage />
---
+++ src/components/Common/Sidebar.tsx
@@ -0,0 +1,39 @@
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export  function Sidebar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <PeopleAltIcon />
+          </ListItemIcon>
+          <ListItemText primary="Students" />
+        </ListItem>
+      </List>
+    </div>
+  );
+}
